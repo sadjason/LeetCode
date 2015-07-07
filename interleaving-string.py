bd442ca44da8eab46654ced59ea760ab1dfcfc5b
sadjason <sadjason@qq.com>
2015-07-08 00:58:43 +0800

    Update Solution
    Update Solution

--- interleaving-string.py
+++ interleaving-string.py
@@ -39,3 +39,45 @@ class Solution:
                 elif aux[i][j-1] and s2[j-1] == s3[i+j-1]:
                     aux[i][j] = True
         return aux[m][n]
+
+
+# 分治法
+class Solution:
+    # @param {string} s1
+    # @param {string} s2
+    # @param {string} s3
+    # @return {boolean}
+    def isInterleave(self, s1, s2, s3):
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+        if n1+n2 != n3:
+            return False
+        return self.aux(s1, 0, s2, 0, s3, 0)
+
+    def aux(self, s1, begin1, s2, begin2, s3, begin3):
+        if begin3 == len(s3):
+            return True
+
+        if begin1 == len(s1):
+            for i in xrange(begin2, len(s2)):
+                if s2[i] != s3[begin3-begin2+i]:
+                    return False
+            return True
+
+        if begin2 == len(s2):
+            for i in xrange(begin1, len(s1)):
+                if s1[i] != s3[begin3-begin1+i]:
+                    return False
+            return True
+
+        c1, c2, c3 = s1[begin1], s2[begin2], s3[begin3]
+        if c1 != c3 and c2 != c3:
+            return False
+        elif c1 == c3 and c2 != c3:
+            return self.aux(s1, begin1+1, s2, begin2, s3, begin3+1)
+        elif c2 == c3 and c1 != c3:
+            return self.aux(s1, begin1, s2, begin2+1, s3, begin3+1)
+        else:
+            b = self.aux(s1, begin1+1, s2, begin2, s3, begin3+1)
+            if not b:
+                b = self.aux(s1, begin1, s2, begin2+1, s3, begin3+1)
+            return b
