35530bad2a524f710bc9607026a0bc8b5076c9d4
sadjason <sadjason@qq.com>
2015-07-14 11:06:49 +0800

    Update Solution
    Update Solution

--- implement-strstr.py
+++ implement-strstr.py
@@ -8,15 +8,58 @@ class Solution:
     # @param needle, a string
     # @return an integer
     def strStr(self, haystack, needle):
-        # haystack - 被查找的字符串，needle - 匹配字符串
-        m, n = len(needle), len(haystack)
+        # haystack - 文本串，needle - 模式串
+        m, n = len(haystack), len(needle)
 
-        for i in xrange(0, n-m+1):
+        for i in xrange(0, m-n+1):
             flag = True
-            for j in xrange(0, m):
+            for j in xrange(0, n):
                 if haystack[i+j] != needle[j]:
                     flag = False
                     break
             if flag:
                 return i
         return -1
+
+
+# KMP算法
+class Solution:
+    # @param haystack, a string
+    # @param needle, a string
+    # @return an integer
+    def strStr(self, haystack, needle):
+        # haystack - 文本串，needle - 模式串
+        m, n = len(haystack), len(needle)
+
+        # 计算next
+        next = self.calNext(needle)
+
+        i = j = 0
+        while i < m and j < n:
+            if haystack[i] == needle[j]:
+                i, j = i+1, j+1
+            else:   # find k and update j
+                if next[j] == -1:
+                    i, j = i+1, 0
+                else:
+                    j = next[j]
+
+        if j == n:
+            return i-n
+        return -1
+
+    def calNext(self, pattern):     # pattern - 模式串
+        n = len(pattern)
+        next = [-1] * n
+        j, k = 0, -1    # j, k = 0, next[0]
+        while j < n-1:
+            if k == -1 or pattern[k] == pattern[j]:
+                j, k = j+1, k+1
+                #next[j] = k
+                if pattern[j] == pattern[k]:
+                    next[j] = next[k]
+                else:
+                    next[j] = k
+            else:
+                k = next[k]
+        return next
